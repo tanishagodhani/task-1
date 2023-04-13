803a32a33ae5f0e84a9b2dc2f290ced613b7c012
tanishagodhani <130463987+tanishagodhani@users.noreply.github.com>
2023-04-13 22:31:49 +0530

    Create task1.c

---
+++ task1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+main()
+{
+
+	printf("\t *\t * \n");
+	printf("    *\t      * \n");
+	printf("*\t * \n");
+	printf("    *\t      *\n");
+	printf("\t *\t *\n");
+	printf("\n");
+	printf("\n");
+	
+	printf("2 \n");
+	printf("\n");
+	printf("    *   *   *   *   *   *   *\n");
+	printf("\n");
+	printf("*   *   *   *   *   *   *   *\n");
+	printf("*\t* \t\t    *\n");
+	printf("*\t* \t*   *   *   *\n");
+	printf("*\t* \t* \t*   *\n");
+	printf("*   *   *   *   *   *   *   *\n");
+	
+	
+}
